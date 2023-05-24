db2f8528edf6f7f8bd7a7203ec269f0834114332
Chris Samar <35326209+kribz920@users.noreply.github.com>
2023-05-24 22:58:34 +0200

    pc update

--- functions/main.py
+++ functions/main.py
@@ -3,3 +3,25 @@ __winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
 __human_name__ = 'functions'
 
 # Add your code after this line
+#def double(x):
+#     doubled_x = x * 2
+#     return doubled_x
+
+# four_doubled = double(4)
+# print(four_doubled)
+
+
+
+def greet(X):
+    greeting = "hello, " + X + "!"
+    return greeting
+
+say_hello = greet("Chris")
+print(say_hello)
+
+def add(a, b, c):
+    sum = a + b + c
+    return sum
+
+som1 = add(4, 5, 6)
+print(som1)
