e9d671525e2e937dba373d55ae65c62613e805e7
Chris Samar <35326209+kribz920@users.noreply.github.com>
2023-06-05 22:33:21 +0200

    list part 1 done

--- lists/main.py
+++ lists/main.py
@@ -3,3 +3,46 @@ __winc_id__ = '6eb355e1a60f48a28a0bbbd0c88d9ab4'
 __human_name__ = 'lists'
 
 # Add your code after this line
+# example_list_of_lists = [['Bob', 3.14, 42], ['Preeti', 3.14, 42]]
+Movie_list = [["The Poseidon Adventure", "Nominated"],
+              ["Cinderella Liberty", "Nominated"],
+              ["Tom Sawyer", "Nominated"],
+              ["Earthquake", "Nominated"],
+              ["Jaws", "Won"],
+              ["Close Encounters of the Third Kind", "Nominated"],
+              ["Star Wars: Episode IV – A New Hope", "Won"],
+              ["Superman", "Nominated"],
+              ["Star Wars: Episode V – The Empire Strikes Back", "Nominated"],
+              ["E.T. the Extra-Terrestrial", "Won"],
+              ["If We Were in Love (from Yes, Giorgio)", "Nominated"],
+              ["The River", "Nominated"],
+              ["Empire of the Sun", "Nominated"],
+              ["The Accidental Tourist", "Nominated"],
+              ["Born on the Fourth of July", "Nominated"],
+              ["Schindler's List", "Nominated"],
+              ["Moonlight (from Sabrina)", "Nominated"],
+              ["Seven Years in Tibet", "Nominated"],
+              ["Saving Private Ryan", "Nominated"],
+              ["Angela's Ashes", "Nominated"],
+              ["A.I. Artificial Intelligence", "Nominated"],
+              ["Memoirs of a Geisha", "Won"],
+              ["War Horse", "Nominated"],
+              ["Lincoln", "Nominated"],
+              ["The Book Thief", "Nominated"],
+              ["The Post", "Nominated"],
+              ["The Fabelmans", "Nominated"]]
+
+
+def  alphabetical_order(movies):
+    sorted_list = sorted(movies)
+    return sorted_list
+
+def won_golden_globe(film_name)
+    film as won a golden globe = True
+    else 
+#     return False
+
+movie_alphabetical_order = (alphabetical_order(Movie_list))
+print(movie_alphabetical_order)
+
+won
