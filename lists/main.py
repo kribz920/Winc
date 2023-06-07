5f3bb4036467b2ad63ba94f209194f4ca26c0e6a
Chris Samar <35326209+kribz920@users.noreply.github.com>
2023-06-07 12:32:07 +0200

    done niet te moeilijk maken van list in list

--- lists/main.py
+++ lists/main.py
@@ -4,39 +4,15 @@ __human_name__ = 'lists'
 
 # Add your code after this line
 # example_list_of_lists = [['Bob', 3.14, 42], ['Preeti', 3.14, 42]]
-Movie_list = [["The Poseidon Adventure", "Nominated"],
-              ["Cinderella Liberty", "Nominated"],
-              ["Tom Sawyer", "Nominated"],
-              ["Earthquake", "Nominated"],
-              ["Jaws", "Won"],
-              ["Close Encounters of the Third Kind", "Nominated"],
-              ["Star Wars: Episode IV – A New Hope", "Won"],
-              ["Superman", "Nominated"],
-              ["Star Wars: Episode V – The Empire Strikes Back", "Nominated"],
-              ["E.T. the Extra-Terrestrial", "Won"],
-              ["If We Were in Love (from Yes, Giorgio)", "Nominated"],
-              ["The River", "Nominated"],
-              ["Empire of the Sun", "Nominated"],
-              ["The Accidental Tourist", "Nominated"],
-              ["Born on the Fourth of July", "Nominated"],
-              ["Schindler's List", "Nominated"],
-              ["Moonlight (from Sabrina)", "Nominated"],
-              ["Seven Years in Tibet", "Nominated"],
-              ["Saving Private Ryan", "Nominated"],
-              ["Angela's Ashes", "Nominated"],
-              ["A.I. Artificial Intelligence", "Nominated"],
-              ["Memoirs of a Geisha", "Won"],
-              ["War Horse", "Nominated"],
-              ["Lincoln", "Nominated"],
-              ["The Book Thief", "Nominated"],
-              ["The Post", "Nominated"],
-              ["The Fabelmans", "Nominated"]]
+Movie_list = ["Jaws", "Star Wars: Episode IV - A New Hope", "E.T. the Extra-Terrestrial", "Memoirs of a Geisha"]
 
-
+# for movie in Movie_list:
+#     Movie_list[movie] = Movie_list[movie].lower()
+# print(Movie_list)
 # Won = filter(lambda c: c[1] == "Won", Movie_list)
-
 # print(list(Won))
-
+for x in range(len(Movie_list)):
+    Movie_list[x] = Movie_list[x].lower()
 
 def  alphabetical_order(movies):
     sorted_list = sorted(movies)
@@ -44,8 +20,7 @@ def  alphabetical_order(movies):
 
 def won_golden_globe(film_name):
     lowercase_film_name = film_name.lower()
-    lowercast_movie_list = list(map(lambda x: list(map(lambda y: y.lower(), x)), Movie_list))
-    if any(lowercase_film_name in sublist for sublist in lowercast_movie_list):
+    if lowercase_film_name in Movie_list:
         return True
     else:
         return False
@@ -53,5 +28,6 @@ def won_golden_globe(film_name):
 movie_alphabetical_order = (alphabetical_order(Movie_list))
 # print(movie_alphabetical_order)
 
-did_movie_won_golden_globe = won_golden_globe("Jaws")
+did_movie_won_golden_globe = won_golden_globe("jawas")
 print(did_movie_won_golden_globe)
+
