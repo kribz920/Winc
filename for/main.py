a1eb244eeeb7b742087c080fca4f34c7cf4ab706
Chris Samar <samar.chris@gmail.com>
2023-09-13 23:10:15 +0200

    1

--- for/main.py
+++ for/main.py
@@ -16,3 +16,32 @@ if __name__ == "__main__":
     countries = get_countries()
 
     """ Write the calls to your functions here. """
+
+def shortest_names(countries):
+    short_names = []
+    len_country_name = [len(country) for country in countries]
+    for country in countries:
+        if len(country) == min(len_country_name):
+            short_names.append(country)
+    return short_names
+
+print(shortest_names(countries))
+
+
+def most_vowels(countries):
+    vowels = "aeiouAEIOU"
+    vowel_counts = []
+    # lower_case_country = [country.lower() for country in countries]
+    for x in countries:
+        count = 0
+        for z in x:
+            if z in vowels:
+                count += 1
+        vowel_counts.append((x, count))
+    #print(vowel_counts)
+    sorted_list = sorted(vowel_counts, key=lambda x: x[1], reverse=True)
+    sorted_strings = [x for x, count in sorted_list][:3]
+    #print(sorted_list)
+    return sorted_strings
+
+print(most_vowels(countries))
