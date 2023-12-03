50291942dc3dd92573cc1b4e669c48447d088b1a
Chris Samar <35326209+kribz920@users.noreply.github.com>
2023-12-03 14:23:59 +0100

    begin aan classes

--- classes/main.py
+++ classes/main.py
@@ -3,3 +3,7 @@ __winc_id__ = '04da020dedb24d42adf41382a231b1ed'
 __human_name__ = 'classes'
 
 # Add your code after this line
+class player():
+    def __init__(self, name, speed, endurance, accuracy):
+        if endurance or accuracy or speed < 0 or endurance or accuracy or speed > 1:
+            raise ValueError("Must be between 0 - 1")
